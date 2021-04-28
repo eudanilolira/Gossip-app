6a2ea2991a557d27635574d67d6d61bda60bd1c6
Danilo Lira <danilo.lira01@gmail.com>
2021-04-27 21:12:36 -0300

    Adiciona tela de registro

--- app/src/main/java/br/ufpe/cin/gossip/MainActivity.kt
+++ app/src/main/java/br/ufpe/cin/gossip/MainActivity.kt
@@ -1,12 +1,14 @@
 package br.ufpe.cin.gossip
 
 import android.Manifest
+import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.wifi.p2p.WifiP2pManager
 import android.net.wifi.p2p.nsd.WifiP2pDnsSdServiceRequest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.view.inputmethod.EditorInfo
 import android.widget.Button
@@ -14,12 +16,14 @@ import android.widget.EditText
 import android.widget.ImageView
 import androidx.core.app.ActivityCompat
 import androidx.core.widget.addTextChangedListener
+import de.hdodenhof.circleimageview.CircleImageView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var profilePicture: ImageView
     private lateinit var userNameEdit: EditText
     private lateinit var confirmButton: Button
+    private lateinit var profilePictureButton: Button
     private var tag: String = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,15 +36,31 @@ class MainActivity : AppCompatActivity() {
 
     private fun startComponents () {
         profilePicture = findViewById(R.id.profilePicture)
+        profilePictureButton = findViewById<Button>(R.id.select_image_circle)
         userNameEdit = findViewById(R.id.userNameEdit)
         confirmButton = findViewById(R.id.confirmButton)
         confirmButton.isEnabled = false
 
     }
 
-    private fun setUpListeners () {
-        profilePicture.setOnClickListener {
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
 
+        if (requestCode == 0 && resultCode == Activity.RESULT_OK && data != null) {
+            val uri = data.data
+            val imageBitmap = MediaStore.Images.Media.getBitmap(contentResolver, uri)
+
+            profilePicture .setImageBitmap(imageBitmap)
+            profilePictureButton.alpha = 0F
+        }
+    }
+
+
+    private fun setUpListeners () {
+        profilePictureButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, 0)
         }
 
         userNameEdit.addTextChangedListener {
